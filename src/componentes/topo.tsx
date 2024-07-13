adb15131b3322d54c40c82dda2176c6ad7a8c118
Bruno Lima <brunolimame@gmail.com>
2024-07-13 17:00:08 -0300

    teste deploy

--- src/componentes/topo.tsx
+++ src/componentes/topo.tsx
@@ -8,7 +8,7 @@ export default function topo() {
     return (
         <Navbar className="py-3" maxWidth="2xl">
             <NavbarBrand className="flex items-center">
-                <Image src={logoTopo} className="max-w-[100%] h-[60px]" />
+                <Image src={logoTopo} className="max-w-[100%] h-[60px]" alt="Peregrinação 2024" />
             </NavbarBrand>
             <NavbarContent className="hidden sm:flex gap-4" justify="end">
                 <NavbarItem>
